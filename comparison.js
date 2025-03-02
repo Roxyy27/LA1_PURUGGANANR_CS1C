39a60d3f64bcb0973ea324a45c499955b9493d2a
Roxyy27 <roxannepurugganan27@gmail.com>
2025-03-02 11:24:24 +0800

    comparison.js

---
+++ comparison.js
@@ -0,0 +1,12 @@
+let firstFavNumb = 7;
+let secondFavNumb = 9;
+
+//logs the results of each comparison to the console
+console.log(firstFavNumb > secondFavNumb);
+console.log(firstFavNumb < secondFavNumb);
+console.log(firstFavNumb >= secondFavNumb);
+console.log(firstFavNumb <= secondFavNumb);
+console.log(firstFavNumb === secondFavNumb);
+console.log(firstFavNumb == secondFavNumb);
+console.log(firstFavNumb !== secondFavNumb);
+console.log(firstFavNumb != secondFavNumb);
