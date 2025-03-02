1cd6ebbf19cad21af55089583787bd113aef262b
Roxyy27 <roxannepurugganan27@gmail.com>
2025-03-02 11:14:34 +0800

    variables.js

---
+++ variables.js
@@ -0,0 +1,9 @@
+let name = 'Roxanne';
+let whatDoYouWannaBecomeInYourLife = 'programmer';
+let gender = 'Female';
+let twitterHandle = 'Roxxxyyyy';
+
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);
